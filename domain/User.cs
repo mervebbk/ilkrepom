7ab8cd81487f18f49c61dc3b7524167f3419bb67
merve <mervebbk9@gmail.com>
2026-08-07 14:07:29 +0300

    Add User class with basic properties

---
+++ domain/User.cs
@@ -0,0 +1,12 @@
+namespace kayıtekranı.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Sifre { get; set; }
+    }
+}
